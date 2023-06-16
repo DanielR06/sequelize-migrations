9beb2dd2a2f327a590a1caedc4d952fd3f991141
Daniel Rodriguez <kungary04@gmail.com>
2023-06-16 18:20:34 -0500

    Migrations create

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    use_env_variable: "DATABASE_URL",
+    logging: false
+  },
+  "test": {
+    use_env_variable: "DATABASE_URL",
+  },
+  "production": {
+    use_env_variable: "DATABASE_URL",
+  }
+}
